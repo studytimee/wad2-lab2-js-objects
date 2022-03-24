d9fefe47e948d18656a031be1bf7f0ecc446a6db
Abid Ali <studytimee@gmail.com>
2022-03-24 12:31:12 +0000

    Looping/Iteration Constructs

--- lab001/fundamentals.js
+++ lab001/fundamentals.js
@@ -1,8 +1,8 @@
 const aCar = {
     owner: "Joe Bloggs",
     previous_owners: [
-        {name: "Alex Smith" ,  address:"- 1 Main Street"},
-        {name: "John Fleming", address:"- 2 High Street "}
+        { name: "Alex Smith", address: "- 1 Main Street" },
+        { name: "John Fleming", address: "- 2 High Street " }
     ],
     type: {
         make: 'Toyota',
@@ -14,10 +14,18 @@ const aCar = {
         stage: '1',
         county: 'WD',
         number: '1058',
+    },
+
+    features: ['parking', 'alarm', 'tow-bar']
+
+};
+
+for (let i = 0; i < aCar.features.length; i++) {
+    console.log(aCar.features[i]);
     }
 
 
-};
+
 
 // Add a new property
 aCar.mileage = '10000';
@@ -28,7 +36,7 @@ aCar.colour = {
         shade: 'Cream',
     }
 }
-console.log('First Owner: ' + aCar.previous_owners[0].name +', Address: '+ aCar.previous_owners[0].address)
+// console.log('First Owner: ' + aCar.previous_owners[0].name +', Address: '+ aCar.previous_owners[0].address)
 
 // console.log(aCar.owner + ' drives a ' + aCar.type.make)
 // console.log(' Car registration : ' + aCar.reg.year + '' + aCar.reg.stage + '-' + aCar.reg.county + '-' + aCar.reg.number)
