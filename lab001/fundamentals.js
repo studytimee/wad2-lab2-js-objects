76512a1bbb2a5e46a3af2453eaddf64efad0e425
Abid Ali <studytimee@gmail.com>
2022-03-24 06:30:38 +0000

    Nested Objects

--- lab001/fundamentals.js
+++ lab001/fundamentals.js
@@ -1,7 +1,11 @@
 const aCar = {
-    owner : "Joe Bloggs",
-    type : 'Toyota Corolla 1.8',
-    registration : '201WD1058'
-  };
-  
-  console.log(aCar.owner);
+    owner: "Joe Bloggs",
+    type: {
+        make: 'Toyota',
+        model: 'Corolla',
+        cc: '1.8',
+    },
+    registration: '201WD1058'
+};
+
+console.log(aCar.owner + ' drives a ' + aCar.type.make)
