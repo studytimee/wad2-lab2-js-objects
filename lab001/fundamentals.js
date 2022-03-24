becc06418721d56db39868320b3c0f60ca93182a
Abid Ali <studytimee@gmail.com>
2022-03-24 07:12:15 +0000

    Dynamic Properties

--- lab001/fundamentals.js
+++ lab001/fundamentals.js
@@ -11,6 +11,26 @@ const aCar = {
         county: 'WD',
         number: '1058',
     }
+
+
 };
-console.log(aCar.owner + ' drives a ' + aCar.type.make )
-console.log(' Car registration : ' + aCar.reg.year+''+aCar.reg.stage+'-'+aCar.reg.county+'-'+aCar.reg.number)
+
+// Add a new property
+aCar.mileage = '10000';
+aCar.colour = {
+    exterior: 'Red',
+    interior: {
+        texture: 'Leather',
+        shade: 'Cream',
+    }
+}
+
+console.log(aCar.owner + ' drives a ' + aCar.type.make)
+console.log(' Car registration : ' + aCar.reg.year + '' + aCar.reg.stage + '-' + aCar.reg.county + '-' + aCar.reg.number)
+
+console.log(' Car Colour : '+aCar.colour.exterior)
+console.log(' Interior Fabric Texture: ' + aCar.colour.interior.texture)
+console.log(' Interior Fabric Shade: ' + aCar.colour.interior.shade)
+
+console.log(' It is a '+ aCar.colour.exterior+' colour, '+aCar.mileage+' mileage, with '+aCar.colour.interior.texture +' interior.')
+
